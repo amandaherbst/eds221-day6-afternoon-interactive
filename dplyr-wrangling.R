16f40ed1c104dbbed05504952a135f5885cfe579
amandaherbst <amandaeherbst@gmail.com>
2023-08-21 15:01:56 -0700

    added select and mutate

--- dplyr-wrangling.R
+++ dplyr-wrangling.R
@@ -57,3 +57,62 @@ large_crabs <- pie_crab %>% filter(size > 13 &
                                      site %in% c("NIB", "CC", "ZI"))
 
 #------- Selecting columns -------#
+
+# Select individual columns by name, separate them by a comma
+crabs_subset <- pie_crab %>% select(latitude, size, water_temp)
+
+# Select a range of columns using:
+crabs_subset2 <- pie_crab %>% select(site:air_temp)
+
+# Select a range and an individual column
+crabs_subset3 <- pie_crab %>% select(date:water_temp, name)
+
+# Can reorder! 
+pie_crab %>% select(name, water_temp, size)
+
+
+#-------- Mutate! -------#
+
+# Use dplyr::mutate() to add or update a column while keeping all existing columns
+
+crabs_cm <- pie_crab %>% 
+  mutate(size_cm = size / 10)
+
+# What happens if I use mutate to add a new column containing the mean of the size column?
+
+crabs_mean <- pie_crab %>% 
+  mutate(size_mean = mean(size, na.rm = TRUE))
+
+# mutate will overwrite a column if you use an already existing name
+crabs_awesome <- pie_crab %>% 
+  mutatue(name = "Teddy is Awesome")
+
+
+# Reminder: group_by and summarize
+mean_size_by_site <- pie_crab %>% 
+  group_by(site) %>% 
+  summarize(mean_size = mean(size, na.rm = TRUE),
+            sd_size = sd(size, na.rm = TRUE))
+
+# What about a group_by and then mutate?
+group_mutate <- pie_crab %>% 
+  group_by(site) %>% 
+  mutate(mean_size = mean(size, na.rm = TRUE))
+
+
+# What if I want to create a new column in pie_crab that contains "giant" if the size is greater than 20, or "not giant" if the size is less than or equal to 35?
+
+# Use dplyr::case_when() to write if-else statements more easily
+crabs_bin <- pie_crab %>% 
+  mutate(size_bin = case_when(
+    size > 20 ~ "giant",
+    size <= 20 ~ "not giant"
+  ))
+
+
+sites_binned <- pie_crab %>% 
+  mutate(region = case_when(
+    site %in% c("ZI", "CC", "PIE") ~ "Low",
+    site %in% c("BB", "NIB") ~ "Middle",
+    TRUE ~ "High" # this is a catch-all meaning make everything else be high
+  ))
